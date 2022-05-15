e334b54f3fa8a3a48c59c5ea121e7da2d36629ec
Gabriel Pimentel <gabriel97pimentel@gmail.com>
2022-05-15 00:16:20 -0300

    Funcionando a conexão mas com o problema do loop dos sockets ociosos

--- conectividade2.py
+++ conectividade2.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import time
 
 
 class Conectividade(threading.Thread):
@@ -7,6 +8,7 @@ class Conectividade(threading.Thread):
         A instância da classe Conectividade é criada a partir do IP e da porta que
         o Host habilita para aguardar conexões.
 
+        *   connected:          True quando uma conexão for realizada
         *   requesting_socket:  Socket responsável por solicitar conexão com o peer
         *   awaiting_socket:    Socket responsável por solicitar conexão com o peer
         *   connection:         awaiting_socket conectado
@@ -15,29 +17,43 @@ class Conectividade(threading.Thread):
     """
 
     def __init__(self, host_ip, host_port):
+
+        super().__init__()
+        self.connected = False
         self.requesting_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.awaiting_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.connection = ''
         self.host_ip = host_ip
         self.host_port = host_port
 
+    """
+        Insiste em se conectar em uma porta específica de outro peer, 
+        caso a conexão seja feita, o outro socket é desabilitado.
+    """
+
     def solicitar_conexao(self, peer_ip, peer_port):
-        while True:
+        while not self.connected:
             try:
                 self.requesting_socket.connect((peer_ip, peer_port))
             except socket.error as e:
                 print("Ainda não conectou")
+                time.sleep(1)
                 continue
-            print("Conectou")
-            break
+        print("solicitaçao encerrada")
+
+    """
+        Aguarda a conexão feita pelo outro peer. Caso a conexão seja realizada,
+         o outro socket é desabilitado.
+    """
 
     def aguardar_conexao(self, host_ip, host_port):
         self.awaiting_socket.bind((host_ip, host_port))
         self.awaiting_socket.listen()
 
+        #self.connected = True
         self.connection, addr = self.awaiting_socket.accept()
-        print(f"rolou {self.connection}")
 
+        print(f"rolou \n{self.connection}")
 
     def enviar_mensagem(self):
         pass
@@ -45,13 +61,19 @@ class Conectividade(threading.Thread):
     def receber_mensagem(self):
         pass
 
+    # esses valores dentro do 'args' foram apenas testes para ver se as conexões estavam funcionando
+    # serviu pra ver se tanto para o socket que estava escutando como para o que
+    # estava tentando estabelecer uma conexao
+
+    # substituir esses valores do arg por (peer_ip, peer_port ,host_port, host_ip)
     def run(self):
-        t_solicitar_conexao = threading.Thread(target=self.solicitar_conexao, args=('127.0.0.1', 55551))
-        t_aguardar_conexao = threading.Thread(target=self.aguardar_conexao, args=('127.0.0.1', 55432))
+        t_solicitar_conexao = threading.Thread(target=self.solicitar_conexao, args=('127.0.0.1', 55431))
+        t_aguardar_conexao = threading.Thread(target=self.aguardar_conexao, args=('127.0.0.1', 45551))
 
         t_solicitar_conexao.start()
         t_aguardar_conexao.start()
 
+
 if __name__ == '__main__':
-    connect = Conectividade('127.0.0.1', 55432)
+    connect = Conectividade('127.0.0.1', 45551)
     connect.run()
